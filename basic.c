9d0b15dbf62358dfc45c0a629ab3617274f40416
Gary Hess <garyhess999@hotmail.com>
2016-09-11 14:04:34 +0300

    basic.c local_var.c

---
+++ basic.c
@@ -0,0 +1,4 @@
+// define empty function that returns an integer
+int my_function() {
+  return 0xbaba;
+}
---
+++ local_var.c
@@ -0,0 +1,5 @@
+// declare a local variable
+int my_function() {
+  int my_var = 0x1234;
+  return my_var;
+}
